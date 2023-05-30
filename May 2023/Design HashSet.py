3727c52590e7797d28daee37f0383124bfeaf9ca
Ovi054 <avi.pal357@gmail.com>
2023-05-30 14:57:55 +0600

    30-05-2023

--- May 2023/Design HashSet.py
+++ May 2023/Design HashSet.py
@@ -22,3 +22,34 @@ class MyHashSet:
 # obj.add(key)
 # obj.remove(key)
 # param_3 = obj.contains(key)
+
+
+
+class MyHashSet:
+
+    def __init__(self):
+        self.map = []
+        self.cap = 1000
+        for i in range(self.cap):
+            self.map.append([])
+
+    def add(self, key: int) -> None:
+        bucket = key % self.cap
+        if key not in self.map[bucket]:
+            self.map[bucket].append(key)
+
+    def remove(self, key: int) -> None:
+        bucket = key % self.cap
+        if key in self.map[bucket]:
+            self.map[bucket].remove(key)
+    def contains(self, key: int) -> bool:
+        bucket = key % self.cap
+        if key in self.map[bucket]:
+            return True
+        else:
+            return False
+# Your MyHashSet object will be instantiated and called as such:
+# obj = MyHashSet()
+# obj.add(key)
+# obj.remove(key)
+# param_3 = obj.contains(key)

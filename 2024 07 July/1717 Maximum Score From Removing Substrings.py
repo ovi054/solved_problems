22ee549c341699e8ea201711dc392bd287fba679
ovi054 <avi.pal357@gmail.com>
2024-07-12 12:17:18 +0600

    12-07-2024

--- 2024 07 July/1717 Maximum Score From Removing Substrings.py
+++ 2024 07 July/1717 Maximum Score From Removing Substrings.py
@@ -1,3 +1,4 @@
+#first solution
 class Solution:
     def maximumGain(self, s: str, x: int, y: int) -> int:
         stack = []
@@ -23,3 +24,27 @@ class Solution:
                 stack.pop()
                 stack.pop()
         return output
+
+
+#improved functionality
+class Solution:
+    def removeSubstring(self, s, subString, addVal):
+        first = subString[0]
+        second = subString[1]
+        stack = []
+        output = 0
+        for char in s:
+            stack.append(char)
+            if len(stack)>1 and stack[-2]==first and stack[-1]==second:
+                output += addVal
+                stack.pop()
+                stack.pop()
+        return ''.join(stack), output
+    def maximumGain(self, s: str, x: int, y: int) -> int:
+        if(x>y):
+            s, output1 = self.removeSubstring(s,"ab",x)
+            s, output2 = self.removeSubstring(s,"ba",y)
+        else:
+            s, output1 = self.removeSubstring(s,"ba",y)
+            s, output2 = self.removeSubstring(s,"ab",x)
+        return output1 + output2

a7dc756d2e8e406f1cb6109291e929c6663f8337
ovi054 <avi.pal357@gmail.com>
2025-01-29 16:01:28 +0600

    29-01-2025

--- 2025 01 January/684 Redundant Connection.py
+++ 2025 01 January/684 Redundant Connection.py
@@ -26,4 +26,38 @@ class Solution:
             graph[a].append(b)
             graph[b].append(a)
 
+
+#Solution 2 DSU
+
+
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+
+        n =len(edges)
+
+        parent = []
+
+        for i in range(n):
+            parent.append(i)
+
+        def find(x):
+            while parent[x] != x:
+                x = parent[x]
+            return x
+
+        def union(x,y):
+            rootX = find(x)
+            rootY = find(y)
+
+            if rootX!=rootY:
+                parent[rootY] = x
+                return True
+            
+            else:
+                return False
+
+        for a, b in edges:
+            if not union(a-1,b-1):
+                return [a,b]
+
             

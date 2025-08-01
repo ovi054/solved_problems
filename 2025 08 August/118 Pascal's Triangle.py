cd6fee5f7f3086d0183328d1890bc0016941820b
ovi054 <avi.pal357@gmail.com>
2025-08-01 11:06:31 +0600

    01-08-2025

---
+++ 2025 08 August/118 Pascal's Triangle.py
@@ -0,0 +1,13 @@
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        output = []
+        for i in range(numRows):
+            output.append([])
+            for j in range(i+1):
+                if j==0 or j==i:
+                    output[i].append(1)
+                else:
+                    output[i].append(output[i-1][j]+output[i-1][j-1])
+
+        return output
+                
